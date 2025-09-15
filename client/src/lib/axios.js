9d6d7bbf6fe1488dda1fdcf5e33ba59c22509964
ajayarumugam73@gmail.com <you@example.com>
2025-09-15 18:24:43 +0530

    fixed one change

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : `${import.meta.env.BACKEND_URL}/api`;
+const BASE_URL = import.meta.env.VITE_NODE === "development" ? "http://localhost:3000/api" : `${import.meta.env.VITE_BACKEND_URL}/api`;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
