d06dfc6486fe80d8962e90e51733e6e4d3be7830
ajayarumugam73@gmail.com <you@example.com>
2025-09-15 18:22:09 +0530

    url updateed

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://streamify-ajay.vercel.app",
         credentials: true, // allow frontend to send cookies
     })
 );
